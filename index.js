720c5f15c0512869300a1ddc1f5f7d17ba40ec4b
Aria Balaei <ariabalayi@gmail.com>
2023-09-13 16:26:13 +0200

    basics

--- index.js
+++ index.js
@@ -19,7 +19,12 @@ const mainCanvas = svg.append('g')
                 .attr('height', graphHeight /2)
                 .attr('width', graphWidth / 2)
                 .attr('transform',`translate(${margin.left},${margin.top})`);
-
+// Color selector
+function colorSelector(amount){
+  if(amount >= 0)
+    return 'green'
+  return 'red'
+}
 
 //CSV Data                
 function getCSVData() {
@@ -29,8 +34,40 @@ function getCSVData() {
 
   getCSVData();
 
-
+const xAxisGroup = mainCanvas.append('g')
+                             .attr('transform', `translate(0, ${graphHeight})`)
+const yAxisGroup = mainCanvas.append('g')
 
 function drawChart(data){
-  console.log(data)
+  
+  const bargraph = mainCanvas.selectAll('g')
+  .data(data)
+  .enter()
+  .append('g')
+
+   //scaleLinear
+   const y = d3.scaleLinear()
+               .domain([0, d3.max(data, d => d.expenses)])
+               .range([graphHeight, 0]);
+ 
+  //scaleBand 
+  const x = d3.scaleBand()
+              .domain()
+              .range([0, 500])
+              .paddingInner(0.15)
+              .paddingOuter(0.15)
+
+  var tickname = data.map(d => d.type)
+  console.log(tickname)
+
+  const yAxis = d3.axisLeft(y);
+  const xAxis = d3.axisBottom(x)
+                  .tickFormat((d,i) => tickname[i])
+
+  const rectOfBars = bargraph.append('rect')
+
+
+  xAxisGroup.call(xAxis);
+  yAxisGroup.call(yAxis);    
+
 }
