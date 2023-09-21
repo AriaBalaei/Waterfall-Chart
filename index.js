ae8c52b9e0c8dad5d44dcb1996eb70fbb29a35ee
Aria Balaei <ariabalayi@gmail.com>
2023-09-21 12:41:38 +0200

    update

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ const margin =
   {
     top: 0,
     right: 60,
-    bottom: 20,
-    left: 60
+    bottom: 200,
+    left: 60,
+    padding: 0.3
   };
 
 
@@ -19,6 +20,8 @@ const mainCanvas = svg.append('g')
                 .attr('height', graphHeight /2)
                 .attr('width', graphWidth / 2)
                 .attr('transform',`translate(${margin.left},${margin.top})`);
+
+
 // Color selector
 function colorSelector(amount){
   if(amount >= 0)
@@ -26,48 +29,52 @@ function colorSelector(amount){
   return 'red'
 }
 
+
+
+
 //CSV Data                
 function getCSVData() {
   d3.csv('/data.csv', function(d){
     return d;
   }).then(drawChart);}
 
-  getCSVData();
-
-const xAxisGroup = mainCanvas.append('g')
-                             .attr('transform', `translate(0, ${graphHeight})`)
-const yAxisGroup = mainCanvas.append('g')
+getCSVData();
 
 function drawChart(data){
+
+  var array_num = [...data.map(d => parseFloat(d.expenses))]
+  var max = d3.max(array_num)
+
+  var firstBar = max
+  var positive = 0
+  var negetive = 0
   
+  function yPlace(d) {
+      if(d < 0)
+        return negetive += d
+      return negetive
+    
+  }
+
+
   const bargraph = mainCanvas.selectAll('g')
   .data(data)
   .enter()
   .append('g')
 
-   //scaleLinear
-   const y = d3.scaleLinear()
-               .domain([0, d3.max(data, d => d.expenses)])
-               .range([graphHeight, 0]);
- 
-  //scaleBand 
-  const x = d3.scaleBand()
-              .domain()
-              .range([0, 500])
-              .paddingInner(0.15)
-              .paddingOuter(0.15)
+  //scaleLinear
+  const y = d3.scaleLinear()
+               .domain([0 ,60])
+               .range([0, graphHeight])
 
   var tickname = data.map(d => d.type)
-  console.log(tickname)
-
-  const yAxis = d3.axisLeft(y);
-  const xAxis = d3.axisBottom(x)
-                  .tickFormat((d,i) => tickname[i])
-
-  const rectOfBars = bargraph.append('rect')
-
-
-  xAxisGroup.call(xAxis);
-  yAxisGroup.call(yAxis);    
-
+ 
+  
+  const rectOfBars = bargraph
+              .append('rect')
+              .attr('width', 20)
+              .attr('height', d => Math.abs(d.expenses/50))
+              .attr('x', (d, i) => i*50)
+              .attr('y', (d) =>  yPlace(d.expenses)/50)
+              .attr('fill', d => colorSelector(d.expenses))
 }
